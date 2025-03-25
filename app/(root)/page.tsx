8b66ca3298c11635c7b9a0a32daf544769561835
jefferson.brandao <jefferson.brandao@g.globo>
2025-03-24 23:53:57 -0300

    criando home page e card de entrevistas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,49 @@
+import InterviewCard from "@/components/interview-card";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>
+            prepare-se para entrevistas com feedbacks práticos e baseados em IA
+          </h2>
 
-export default HomePage
+          <p className="text-lg">
+            pratique perguntas de entrevista reais e obtenha feedbacks
+            instantâneos
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/entrevista">Começar a entrevista</Link>
+          </Button>
+        </div>
+
+        <Image src={"/robot.png"} alt="Robo-dude" width={400} height={400} className="max-sm:hidden" />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Suas entrevistas</h2>
+
+        <div className="interviews-section">
+          <InterviewCard />
+
+          <p>Você não tem entrevistas no momento</p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Faça uma entrevista</h2>
+
+        <div className="interviews-section">
+          <p>Não existe entrevistas no momento</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
